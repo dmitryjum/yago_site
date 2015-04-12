744772364ab69b0f51aac5f885f8aa3d7dab3de7
Dmitry Jum <xfiler@ymail.com>
2015-04-12 19:20:18 -0400

    adjusted masonry columns and image widths

--- js/directives.js
+++ js/directives.js
@@ -11,7 +11,7 @@ myApp.directive('masonry',function() {
         if (oldVal !== newVal) {
           var container = document.getElementById("container");
           var msnry = new Masonry( container, {
-            columnWidth: 250,
+            columnWidth: 230,
             gutter: 3,
             itemSelector: '.item'
           });
