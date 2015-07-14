cb2d97392681fa6453dc4da7f874b59a3a1e927a
Dmitry Jum <xfiler@ymail.com>
2015-07-14 01:11:14 -0400

    added siSubmitting to personal form

--- js/controllers/admin_personal_controller.js
+++ js/controllers/admin_personal_controller.js
@@ -2,8 +2,11 @@ angular.module('YagoApp').controller('AdminPersonalController', ['$scope', 'Pers
  function($scope, Personal) {
   // apply admin.js file here as well
   $scope.info = Personal("info");
+  $scope.isSubmitting = false;
   $scope.saveInfo = function() {
+    $scope.isSubmitting = true;
       $scope.info.$save().then(function() {
+        $scope.isSubmitting = false;
         alert('Profile saved!');
       }).catch(function(error) {
         alert('Error!');
