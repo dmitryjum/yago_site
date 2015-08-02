2bbbcd5a13445290f819722b3ba6cc96621937da
Dmitry Jum <xfiler@ymail.com>
2015-08-01 21:22:30 -0400

    added google analytics page tracking code

--- js/controllers/videos_controller.js
+++ js/controllers/videos_controller.js
@@ -1,6 +1,9 @@
-angular.module('YagoApp').controller('VideoController', ['$scope', '$css', 'Video', 'Personal',
- function($scope, $css, Video, Personal) {
+angular.module('YagoApp').controller('VideoController', ['$scope', '$window', '$css', 'Video', 'Personal',
+ function($scope, $window, $css, Video, Personal) {
   $scope.videos = Video;
   Personal("info").$loaded().then(function(data) { $scope.info = data; });
   $scope.isCollapsed = true;
+  $scope.$on('$viewContentLoaded', function(event) {
+    $window.ga('send', 'pageview');
+  });
 }]);
