25b524c92295665f395ba350b9a5ebf91c04cb78
Dmitry Jum <xfiler@ymail.com>
2015-08-21 00:28:06 -0400

    grunt server works, attempting to access google analytics api using their example, some auth error and unable to query accounts to get account id

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,9 +11,9 @@ module.exports = function(grunt) {
         }
       }
     },
-    jshint: {
-      all: [ 'Gruntfile.js', 'js/*.js', 'js/**/*.js' ]
-    },
+    // jshint: {
+    //   all: [ 'Gruntfile.js', 'js/*.js', 'js/**/*.js' ]
+    // },
     karma: {
       options: {
         configFile: 'config/karma.conf.js'
@@ -27,12 +27,12 @@ module.exports = function(grunt) {
         autoWatch: true
       }
     },
-    html2js: {
-      dist: {
-        src: [ 'partials/*.html' ],
-        dest: 'tmp/templates.js'
-      }
-    },
+    // html2js: {
+    //   dist: {
+    //     src: [ 'partials/*.html' ],
+    //     dest: 'tmp/templates.js'
+    //   }
+    // },
     concat: {
       options: {
         separator: ';'
@@ -51,16 +51,49 @@ module.exports = function(grunt) {
           mangle: false
         }
       }
+    },
+    clean: {
+      temp: {
+        src: [ 'tmp' ]
+      }
+    },
+    watch: {
+      dev: {
+        files: [ 'Gruntfile.js', 'js/*.js', '*.html' ],
+        tasks: [ 'karma:unit', 'concat:dist', 'clean:temp' ],
+        options: {
+          atBegin: true
+        }
+      },
+      min: {
+        files: [ 'Gruntfile.js', 'js/*.js', '*.html' ],
+        tasks: [ 'karma:unit', 'concat:dist', 'clean:temp', 'uglify:dist' ],
+        options: {
+          atBegin: true
+        }
+      }
+    },
+    connect: {
+      server: {
+        options: {
+          hostname: 'localhost',
+          port: 8080
+        }
+      }
     }
   });
-  grunt.loadNpmTasks('grunt-contrib-jshint');
+  // grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-contrib-connect');
-  grunt.loadNpmTasks('grunt-contrib-compress');
+  // grunt.loadNpmTasks('grunt-contrib-compress');
   grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-html2js');
+  // grunt.loadNpmTasks('grunt-html2js');
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-bower-task');
   grunt.loadNpmTasks('grunt-karma');
+
+  grunt.registerTask('dev', [ 'bower', 'connect:server', 'watch:dev' ]);
+  // grunt.registerTask('test', [ 'bower', 'jshint', 'karma:continuous' ]);
+  grunt.registerTask('minified', [ 'bower', 'connect:server', 'watch:min' ]);
 };
